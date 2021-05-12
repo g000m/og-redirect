99d86abd408842d58aa4832d2e1446e882208d32
Gabe Herbert <gh@gabeherbert.com>
2021-05-12 11:49:44 -0700

    improves support for actual 404's

--- og-redirect.php
+++ og-redirect.php
@@ -13,27 +13,31 @@
  */
 
 add_filter('old_slug_redirect_post_id', function ($id) {
-    global $og_url;
-    $og_url = get_post_canonical_url_meta($id);
-
-    return $id;
-});
-
-function reset_404($link) {
-	$test = false;
-	if (is_404() && is_FB() || $test) {
-		global $og_url, $wp_query;
-
-		$wp_query->is_single = true;
-		$wp_query->is_404    = false;
-		status_header(200);
-
-//        $og_url = $link;
-
-		return null;
+	if ($id <=0) {
+        return $id;
 	}
 
-	return $link;
+    global $og_url;
+    $og_url = (!empty(get_post_canonical_url_meta($id))) ? get_post_canonical_url_meta($id) : $og_url;
+
+});
+
+function reset_404($link)
+{
+    $test = false;
+    if (is_404() && is_FB() || $test) {
+        global $og_url, $wp_query;
+
+        $wp_query->is_single = true;
+        $wp_query->is_404    = false;
+        status_header(200);
+
+        $og_url = $link;
+
+        return null;
+    }
+
+    return $link;
 }
 add_filter('old_slug_redirect_url', 'reset_404');
 
@@ -55,15 +59,21 @@ function get_post_canonical_url_meta(int $post_id) : string
 function og_meta()
 {
     global $og_url, $post;
-    if (empty($og_url)) {
-        $url = get_post_canonical_url_meta($post->ID);
-    } else {
+    if (is_null($post)) {
+		return;
+    }
+
+    $post_canonical_url_meta = get_post_canonical_url_meta( $post->ID );
+    if (isset($og_url)) {
         $url = $og_url;
+    } elseif (!empty($post_canonical_url_meta)) {
+	    $url = $post_canonical_url_meta;
+    } else {
+	    $url = get_permalink( $post );
     }
     echo "\n<meta property=\"og:url\" content=\"$url\" />";
     echo "\n<meta property=\"og:type\" content=\"article\" />\n";
 }
-
 add_action('wp_head', 'og_meta');
 
 
@@ -78,11 +88,14 @@ add_action('wp_head', 'og_meta');
  */
 function _get_post_canonical_url_meta($permalink, $post, $leavename)
 {
-    if (has_caller_method('wpfc_show_facebook_comments')) {
-        return get_post_canonical_url_meta($post->ID);
-    }
+	if ( has_caller_method( 'wpfc_show_facebook_comments' ) ) {
+		$url = get_post_canonical_url_meta( $post->ID );
+		if ( ! empty( $url ) ) {
+			return $url;
+		}
+	}
 
-    return $permalink;
+	return $permalink;
 }
 
 add_filter('post_link', '_get_post_canonical_url_meta', 10, 3);
@@ -104,15 +117,15 @@ function has_caller_method(string $method)
 
 function handle_404($preempt, $wp_query)
 {
-	if (true || $_SERVER['HTTP_USER_AGENT'] === "FacebookExternalHit") {
-		global $wp_query;
+    if (true || $_SERVER['HTTP_USER_AGENT'] === "FacebookExternalHit") {
+        global $wp_query;
 
 //      $wp_query->set_404();
 
-		return true;
-	}
+        return true;
+    }
 
-	return $preempt;
+    return $preempt;
 }
 
 //add_filter( 'pre_handle_404', 'handle_404', 10, 2 );
