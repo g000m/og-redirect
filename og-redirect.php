c4883271a25ab3f04045530be92259d5664d2e57
Gabe Herbert <gh@gabeherbert.com>
2021-05-12 14:02:23 -0700

    refactors too much for one commit really

--- og-redirect.php
+++ og-redirect.php
@@ -9,37 +9,83 @@
  * Domain Path:     /languages
  * Version:         0.1.0
  *
- * @package         Og_Redirect
+ * @package         OG_Redirect
  */
 
-add_filter('old_slug_redirect_post_id', function ($id) {
-	if ($id <=0) {
-        return $id;
-	}
-
-    global $og_url;
-    $og_url = (!empty(get_post_canonical_url_meta($id))) ? get_post_canonical_url_meta($id) : $og_url;
-
-});
-
-function reset_404($link)
+/**
+ * Class OG_Redirect
+ *
+ * This handles 3 cases:
+ *
+ * - invalid URL/404
+ * - valid original URL
+ * - valid redirected URL
+ */
+class OG_Redirect
 {
-    $test = false;
-    if (is_404() && is_FB() || $test) {
-        global $og_url, $wp_query;
+    public $og_url = null;
 
-        $wp_query->is_single = true;
-        $wp_query->is_404    = false;
-        status_header(200);
+    public $post = null;
 
-        $og_url = $link;
+    function __construct()
+    {
+	    global $post;
 
-        return null;
+        add_filter('old_slug_redirect_post_id', array( $this, 'set_og_url' ));
+        add_filter('old_slug_redirect_url', array( $this, 'reset_404' ));
+        add_action('wp_head', array( $this, 'og_meta' ));
     }
 
-    return $link;
+    public function set_og_url($id)
+    {
+        if ($id <= 0) {
+            return $id;
+        }
+
+        $this->post = get_post($id);
+//        $this->og_url = ( ! empty(get_post_canonical_url_meta($id)) ) ? get_post_canonical_url_meta($id) : $this->og_url;
+
+        return $id;
+    }
+
+    public function reset_404($link)
+    {
+        $test = false;
+        if (is_404() && (is_FB() || $test)) {
+            global $wp_query;
+
+            $wp_query->is_single = true;
+            $wp_query->is_404    = false;
+            status_header(200);
+
+//          $this->og_url = $link;
+
+            return null;
+        }
+
+        return $link;
+    }
+
+    public function og_meta()
+    {
+    	global $post;
+        if (is_null($post)) {
+            return;
+        }
+
+        $post_canonical_url_meta = get_post_canonical_url_meta($post->ID);
+        if (isset($this->og_url)) {
+            $url = $this->og_url;
+        } elseif (! empty($post_canonical_url_meta)) {
+            $url = $post_canonical_url_meta;
+        } else {
+            $url = get_permalink($post);
+        }
+        echo "\n<meta property=\"og:url\" content=\"$url\" />";
+        echo "\n<meta property=\"og:type\" content=\"article\" />\n";
+    }
 }
-add_filter('old_slug_redirect_url', 'reset_404');
+
 
 
 
@@ -56,25 +102,6 @@ function get_post_canonical_url_meta(int $post_id) : string
     return reset($values);
 }
 
-function og_meta()
-{
-    global $og_url, $post;
-    if (is_null($post)) {
-		return;
-    }
-
-    $post_canonical_url_meta = get_post_canonical_url_meta( $post->ID );
-    if (isset($og_url)) {
-        $url = $og_url;
-    } elseif (!empty($post_canonical_url_meta)) {
-	    $url = $post_canonical_url_meta;
-    } else {
-	    $url = get_permalink( $post );
-    }
-    echo "\n<meta property=\"og:url\" content=\"$url\" />";
-    echo "\n<meta property=\"og:type\" content=\"article\" />\n";
-}
-add_action('wp_head', 'og_meta');
 
 
 /**
@@ -88,14 +115,14 @@ add_action('wp_head', 'og_meta');
  */
 function _get_post_canonical_url_meta($permalink, $post, $leavename)
 {
-	if ( has_caller_method( 'wpfc_show_facebook_comments' ) ) {
-		$url = get_post_canonical_url_meta( $post->ID );
-		if ( ! empty( $url ) ) {
-			return $url;
-		}
-	}
+    if (has_caller_method('wpfc_show_facebook_comments')) {
+        $url = get_post_canonical_url_meta($post->ID);
+        if (! empty($url)) {
+            return $url;
+        }
+    }
 
-	return $permalink;
+    return $permalink;
 }
 
 add_filter('post_link', '_get_post_canonical_url_meta', 10, 3);
@@ -129,3 +156,5 @@ function handle_404($preempt, $wp_query)
 }
 
 //add_filter( 'pre_handle_404', 'handle_404', 10, 2 );
+
+new OG_Redirect();
