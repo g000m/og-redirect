2e7a309c4aefa3c4b06d96a769285802294f9a78
Gabe Herbert <gh@gabeherbert.com>
2021-05-12 17:01:25 -0700

    disables test variable

--- og-redirect.php
+++ og-redirect.php
@@ -55,7 +55,7 @@ class OG_Redirect
      */
     public function reset_404($link)
     {
-        $test = true;
+        $test = false;
         if (is_404() && (is_FB() || $test)) {
             global $wp_query;
 
