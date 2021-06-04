de20964f8d3f06f34f4582da821b56c4095f37b3
Gabe Herbert <gh@gabeherbert.com>
2021-06-04 13:50:22 -0700

    refactors og_post to use env's

--- include/class-og-post.php
+++ include/class-og-post.php
@@ -2,26 +2,34 @@
 
 namespace GHC;
 
-require_once __DIR__ . '../vendor/autoload.php'; // change path as needed
+if (! defined('ABSPATH')) {
+//	return;
+	// @TODO remove test code
+	require_once dirname(__DIR__) . '/../../../../vendor/autoload.php';
+	require_once dirname(__DIR__) . '/../../../../config/application.php';
+}
+
+require_once __DIR__ . '/../vendor/autoload.php'; // change path as needed
 
 class OG_Post {
 	private string $canonical_url_meta_key = 'og_canonical_url';
 
-	private object $post;
+//	private object $post;
 
 	protected \Facebook\Facebook $fb;
+	private \Facebook\GraphNodes\GraphNode $graph_node;
 
 	function __construct( $post = null ) {
 		$this->post = $post;
 	}
 
-	public function init_sdk(string $app_id, string $app_secret, string $default_token) {
+	public function init_sdk() {
 
 		$this->fb = new \Facebook\Facebook([
-			'app_id' => $app_id,
-			'app_secret' => $app_secret,
+			'app_id' => FB_APP_ID,
+			'app_secret' => FB_APP_SECRET,
 			'default_graph_version' => 'v10.0',
-			'default_access_token' => $default_token, // optional
+			'default_access_token' => FB_BEARER_TOKEN, // optional
 		]);
 	}
 
@@ -44,7 +52,6 @@ class OG_Post {
 			$response = $this->fb->post(
 				$endpoint,
 				$request,
-				'{access-token}'
 			);
 		} catch(FacebookExceptionsFacebookResponseException $e) {
 			echo 'Graph returned an error: ' . $e->getMessage();
@@ -52,8 +59,11 @@ class OG_Post {
 		} catch(FacebookExceptionsFacebookSDKException $e) {
 			echo 'Facebook SDK returned an error: ' . $e->getMessage();
 			exit;
+		} catch(Exception $e) {
+			echo 'Exception: ' . $e->getMessage();
+			exit;
 		}
-		$this->graphNode = $response->getGraphNode();
+		$this->graph_node = $response->getGraphNode();
 	}
 
 	protected function get( string $endpoint = '/' ) {
@@ -67,11 +77,11 @@ class OG_Post {
 			echo 'Facebook SDK returned an error: ' . $e->getMessage();
 			exit;
 		}
-		$this->graphNode = $response->getGraphNode();
+		$this->graph_node = $response->getGraphNode();
 	}
 }
 
 $og = new OG_Post();
-$og->init_sdk('app_id', 'app_secret', 'token' );
+$og->init_sdk();
 $og->scrape('https://360.devhost.us/2021/05/12/slug-of-three/');
 
